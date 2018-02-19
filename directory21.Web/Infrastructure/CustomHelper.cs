c896913691e59b4a07812b39a72692d8d990bc4b
Abusaleh Md Nayeem <nayeemmbstu@gmail.com>
2018-02-19 17:53:17 +0600

    added new helper

--- directory21.Web/Infrastructure/CustomHelper.cs
+++ directory21.Web/Infrastructure/CustomHelper.cs
@@ -4,6 +4,26 @@ namespace directory21.Web.Infrastructure
 {
     public static class CustomHelper
     {
-        
+        public static HtmlEmpyEnd DefaultBox(this HtmlHelper html,
+            string heading, string boxClass = "box-default")
+        {
+            boxClass = "box " + boxClass + " box-solid";
+            html.ViewContext.Writer.Write(
+                "<div class=\"" +
+                boxClass +
+                "\">" +
+                "<div class=\"box-header with-border\">" +
+                "<h3 class=\"box-title\">" +
+                heading +
+                "</h3>" +
+                "<div class=\"box-tools pull-right\">" +
+                "<button type=\"button\" class=\"btn btn-box-tool\" data-widget=\"collapse\"><i class=\"fa fa-minus\"></i></button>" +
+                "</div>" +
+                "</div>" +
+                "<div class=\"box-body\">"
+            );
+
+            return new HtmlEmpyEnd(html.ViewContext);
+        }
     }
 }
---
+++ directory21.Web/Infrastructure/HtmlEmpyEnd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace directory21.Web.Infrastructure
+{
+    public class HtmlEmpyEnd : IDisposable
+    {
+        private readonly ViewContext _viewContext;
+        public HtmlEmpyEnd(ViewContext viewContext)
+        {
+            _viewContext = viewContext;
+        }
+        public void Dispose()
+        {
+            _viewContext.Writer.Write("");
+        }
+    }
+}
