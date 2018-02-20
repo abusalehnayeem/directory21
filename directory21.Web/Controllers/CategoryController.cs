22278ca9ca00f25288e1ae35fd9cd15053ce3c56
Abusaleh Md Nayeem <nayeemmbstu@gmail.com>
2018-02-21 01:39:14 +0600

    Added category controller

---
+++ directory21.Web/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace directory21.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
