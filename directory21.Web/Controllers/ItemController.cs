28006060d06dd9673892676c7c01184dbc33fd99
Abusaleh Md Nayeem <nayeemmbstu@gmail.com>
2018-02-21 02:04:12 +0600

    Added Item Controller

---
+++ directory21.Web/Controllers/ItemController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace directory21.Web.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
